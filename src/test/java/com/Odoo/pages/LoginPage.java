8c3bfab81d3e01dc0e86dcc50e10ec9fe347f185
Muhammed Kucuk <mkucuk2314@gmail.com>
2020-01-04 20:38:53 -0600

    Added LoginPage

---
+++ src/test/java/com/Odoo/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package com.Odoo.pages;
+
+import com.Odoo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+    @FindBy(id="login")
+    public WebElement userName;
+
+    @FindBy(id="password")
+    public WebElement passWord;
+
+    @FindBy(xpath ="//*[@id=\"wrapwrap\"]/main/div/form/div[3]/button")
+    public WebElement submit;
+
+    public static void main(String[] args) {
+        Driver.get().get("http://34.220.250.213/web/login");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.userName.sendKeys("expensesmanager44@info.com");
+        loginPage.passWord.sendKeys("expensesmanager");
+        loginPage.submit.click();
+
+    }
+}
