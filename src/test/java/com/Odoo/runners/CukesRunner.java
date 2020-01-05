808464c85ba71166bf93de3653b6c33c83c4c7e9
Muhammed Kucuk <mkucuk2314@gmail.com>
2020-01-04 19:48:46 -0600

    initial commit

---
+++ src/test/java/com/Odoo/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.Odoo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {"json:target/cucumber.json"},
+        features = "src/test/resources/features",
+        glue = "com.Odoo.stepdefinitions",
+        dryRun = true
+
+)
+
+public class CukesRunner {
+}
