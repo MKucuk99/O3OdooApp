1cac177d5342a5ffb6ffc5f96e5a97d8c4afa74c
Maral312 <maral@nuriles.com>
2020-01-07 16:41:02 -0600

    added 3 directories under features. please create your feature class under related files

--- src/test/java/com/Odoo/stepdefinitions/CRM_CreateStepDefinitions.java
+++ src/test/java/com/Odoo/stepdefinitions/CRM_CreateStepDefinitions.java
@@ -8,13 +8,16 @@ public class CRM_CreateStepDefinitions {
 
     @Given("user is on login page")
     public void userIsOnLoginPage() {
+        System.out.println("I am on login page!");
     }
 
     @Then("user logs as an events crmManager")
     public void userLogsAsAnEventsCrmManager() {
+        System.out.println("Login as an events crmManager");
     }
 
     @And("user verifies that {string} button is clickable")
-    public void userVerifiesThatButtonIsClickable(String arg0) {
+    public void userVerifiesThatButtonIsClickable(String string) {
+        System.out.println("Verifying that "+ string+ " button is clickable");
     }
 }
